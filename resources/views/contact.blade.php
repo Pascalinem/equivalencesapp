228d3f7b2886b7e082b897eb019fd6f6b09b519b
pascaline <mbida.pascaline@gmail.com>
2022-10-17 03:34:57 +0200

    Layouts et partials

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<p> formulaire à créer</p>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,8 @@
+
+<ul> 
+    <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+    <li><a href="#">Messages</a></li>
+    <li><a href="{{ route('contact') }}">Formulaire de contact</a></li>
+    <li><a href='#'>test</a></li>
+
+</ul>
