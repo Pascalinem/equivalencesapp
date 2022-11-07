c8d6b1d92f94e27ea5f5214612372dbd1d3da115
pascaline <mbida.pascaline@gmail.com>
2022-11-07 10:21:35 +0100

    Tailwindcss config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Equivalence des diplômes</title>
         @livewireStyles
+        @vite('resources/css/app.css')
     </head>
 
     <body>
