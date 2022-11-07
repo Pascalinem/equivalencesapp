63893d5f17587329a81603ea1ae5a4784b71c252
pascaline <mbida.pascaline@gmail.com>
2022-11-07 09:05:09 +0100

    Installation et configuration Fortify+ login + register

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h2>
+    salut{{ auth()->user()->name }}
+</h2>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->name('welcome');
 });
-Route::get('/',[DemandeController::class,'index'])->name('dashboard');
-Route::get('demandes/{id}',[DemandeController::class,'show'])->name('demande');
-Route::get('/contactez-nous',[DemandeController::class,'contact'])->name('contact');
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth')->name('home');
+
+
+//Route::get('demandes/{id}',[DemandeController::class,'show'])->name('demande');
+//Route::get('/contactez-nous',[DemandeController::class,'contact'])->name('contact');
 
 //Route::get('demandes/{id}',[DemandeController::class,'demande']);
